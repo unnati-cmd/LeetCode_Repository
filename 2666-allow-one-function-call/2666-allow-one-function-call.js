c7c226f4cfa2521ed16d8290007a557be45fc8fb
Unnati Mishra <unnatiald@gmail.com>
2026-06-28 22:49:36 +0530

    Time: 35 ms (93.85%), Space: 55.1 MB (6.21%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,15 @@
+var once = function(fn) {
+
+    let called = false;
+
+    return function(...args) {
+
+        if(called){
+            return undefined;
+        }
+
+        called = true;
+
+        return fn(...args);
+    }
+};

8b31c76132d5ccc3d8380854aa8165236b2e24bc
Unnati Mishra <unnatiald@gmail.com>
2026-06-28 22:46:11 +0530

    Time: 53 ms (39.58%), Space: 56.5 MB (61.35%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var org = init;
+    return{
+        increment: ()=> {
+            init = init + 1;
+            return init;
+        },
+        decrement: ()=> {
+            init -=1;
+            return init;
+        },
+        reset: ()=> {
+            init = org;
+            return init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */

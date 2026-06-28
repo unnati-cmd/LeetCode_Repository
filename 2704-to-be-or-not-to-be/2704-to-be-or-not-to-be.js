ab750c6fa2c75a2ca00976ad2e9498284cbd94ce
Unnati Mishra <unnatiald@gmail.com>
2026-06-28 22:45:45 +0530

    Time: 31 ms (98.09%), Space: 52.7 MB (91.44%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(num){
+            if(num === val)
+                return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(num){
+            if(num !== val)
+                return true;
+            throw new Error("Equal");
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */

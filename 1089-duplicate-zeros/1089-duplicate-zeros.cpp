894b4dbaf450640027f986057c6ba60c66e06b03
Unnati Mishra <unnatiald@gmail.com>
2026-06-30 00:06:05 +0530

    Time: 5 ms (24.64%), Space: 14.1 MB (32.9%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
         for(int i = 0; i<n; i++){
             if(arr[i]==0) {
-                arr.insert(arr.begin()+i+1,0);
+                arr.insert(arr.begin()+i+1,1,0);
                 i++;
             }
         }

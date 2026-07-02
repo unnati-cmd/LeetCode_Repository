a102598bcd03c3802e9d735da75759bbbdb4e2d9
Unnati Mishra <unnatiald@gmail.com>
2026-07-02 23:53:50 +0530

    Time: 59 ms (12.86%), Space: 53.2 MB (91.05%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"), t);
+        });
+
+        return Promise.race([
+            fn(...args),
+            timeoutPromise
+        ]);
+    };
+};

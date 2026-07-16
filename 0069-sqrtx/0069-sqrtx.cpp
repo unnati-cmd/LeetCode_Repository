8e8c1a6f6766c4e21e9c39b77abe861a4ed688ea
Unnati Mishra <unnatiald@gmail.com>
2026-07-16 20:43:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
-    int mySqrt(int x, int a = 1, int i = 0) {
-        if(x<0) return i-1;
-        if(x == 0) return i;
-        return mySqrt(x-a, a+2, i + 1);
+    int mySqrt(int x) {
+        long long low = 1, high = x;
+        bool flag = false;
+        while(low<=high){
+            long long mid = low + abs(low-high)/2;
+            if(mid*mid == x) {
+                flag = true;
+                return mid;
+            }
+            else if(mid*mid > x) high = mid - 1;
+            else low = mid + 1;
+        }
+        if(!flag) return high;
+        return 0;
     }
 };

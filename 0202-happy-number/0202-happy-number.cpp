4a7ef504fe2e53973f5fcdb6328f3c35b91de850
Unnati Mishra <unnatiald@gmail.com>
2026-07-17 11:31:21 +0530

    Time: 2 ms (23.31%), Space: 8.3 MB (52.69%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        int res = n;
+    bool isHappy(int res) {
         unordered_map<int,int> mp;
-        mp[res]++;
         while(true){
-            int num = 0;
+            mp[res]++;
+            int sum = 0;
             while(res){
-                num += pow(res%10,2);
+                sum += pow(res%10,2);
                 res/=10;
             }
-            if(num == 1) return true;
-            if(mp.find(num)!=mp.end()) return false;
-            mp[num]++;
-            res=num;
+            if(sum == 1) return true;
+            if(mp.find(sum)!=mp.end()) return false;
+            res = sum;
         }
         return false;
     }

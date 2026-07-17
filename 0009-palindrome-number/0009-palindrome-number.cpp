af812037b5bf64f2487d88f950ed22f28bdf9030
Unnati Mishra <unnatiald@gmail.com>
2026-07-17 11:00:21 +0530

    Time: 7 ms (14.06%), Space: 8.7 MB (37.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        long long new_num = 0;
-        long long old = x;
-        while(x){
-            new_num = new_num*10 + (x%10);
+        if(x<0 || (x%10==0) & x!=0) return false;
+        int rev = 0;
+        while(x>rev){
+            rev = rev*10 + x%10;
             x/=10;
         }
-        if(old == new_num) return true;
-        return false;
+        return x==rev || x == rev/10;
     }
 };

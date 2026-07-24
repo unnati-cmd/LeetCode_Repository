4c1ad0b121a921ac8846d3b30192d9a8f7c7d319
Unnati Mishra <unnatiald@gmail.com>
2026-07-24 14:58:29 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.97%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long exp = n;
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        double ans = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1)
+                ans *= x;
+            x *= x;
+            exp /= 2;
+        }
+        return ans;
+    }
+};

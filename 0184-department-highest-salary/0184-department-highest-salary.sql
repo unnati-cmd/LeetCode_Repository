b35cdc2d56a46c04506cfa909a6188d7fc93aaf7
Unnati Mishra <unnatiald@gmail.com>
2026-08-13 10:53:58 +0530

    Time: 754 ms (54.3%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,8 +1,18 @@
-/* Write your T-SQL query statement below */
-select t.Department, t.Employee, t.Salary
-from (select d.name as Department, e.name as Employee, e.salary,
-dense_rank() over(partition by departmentId order by salary desc) as dt
-from Employee e
-join Department d
-on e.departmentId = d.id) t
-where dt = 1;
+# Write your MySQL query statement below
+SELECT t.Department,t.Employee,t.Salary
+FROM
+(
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary,
+        Dense_rank()
+        OVER(
+            PARTITION BY departmentId
+            ORDER BY salary DESC
+        ) AS highest_employee
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+) t
+WHERE highest_employee = 1;

8b3fa37a9e47b03264e6e2dc2fa438f916abdede
Unnati Mishra <unnatiald@gmail.com>
2026-08-14 19:05:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.53%) - LeetHub

--- 3861-minimum-capacity-box/3861-minimum-capacity-box.py
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.py
@@ -1,12 +1,12 @@
 class Solution:
     def minimumIndex(self, capacity: list[int], itemSize: int) -> int:
-        l = []
+        l = 0
         mini = float('inf')
         for i in capacity:
             if(i>=itemSize):
-                l.append(capacity.index(i))
+                l+=1
                 mini = min(mini,i)
-        if(len(l)):
+        if(l):
             return capacity.index(mini)
         else:
             return -1

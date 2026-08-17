e838947c6630b21e1c4c5ab811dbcda775122260
Unnati Mishra <unnatiald@gmail.com>
2026-08-17 15:22:58 +0530

    Time: 11 ms (26.32%), Space: 20.3 MB (87.49%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        count = collections.Counter(nums)
+        for i in count:
+            if(count[i]>1):
+                return i
+        return -1

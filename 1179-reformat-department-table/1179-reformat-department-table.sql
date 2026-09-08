1a3c1ddd57cb01f913c195a8b176f3d0cacd88c8
Unnati Mishra <unnatiald@gmail.com>
2026-09-08 11:29:37 +0530

    Time: 954 ms (42.72%), Space: 0B (100%) - LeetHub

---
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -0,0 +1,15 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    id, 
+    Jan as Jan_Revenue, Feb as Feb_Revenue, Mar as Mar_Revenue, Apr as Apr_Revenue, May as May_Revenue, Jun as Jun_Revenue, Jul as Jul_Revenue, Aug as Aug_Revenue, Sep as Sep_Revenue, Oct as Oct_Revenue, Nov as Nov_Revenue, Dec as Dec_Revenue
+FROM 
+    (
+        SELECT id, month, revenue 
+        FROM Department
+    ) AS SourceData
+PIVOT 
+    (
+        sum(revenue)
+        FOR [month] 
+        IN ([Jan], [Feb], [Mar], [Apr], [May], [Jun], [Jul], [Aug], [Sep], [Oct], [Nov], [Dec]) 
+    ) AS PivotTable;

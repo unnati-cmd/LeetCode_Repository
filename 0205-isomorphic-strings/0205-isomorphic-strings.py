3d82b006471da1c3657dfcc37dcee307364361b4
Unnati Mishra <unnatiald@gmail.com>
2026-09-11 16:04:42 +0530

    Time: 7 ms (39.02%), Space: 19.2 MB (82.99%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,21 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if(len(s)!=len(t)):
-            return False
-        temp = ""
-        d={}
-        used = set()
-        for i in range(len(s)):
-            if(s[i] in d):
-                if d[s[i]]!=t[i]:
-                    return False
-            else:
-                if(t[i] in used):
-                    return False
-                d[s[i]] = t[i]
-                used.add(t[i])
+            return false
+        d = {}
+        j = 0
         for i in s:
-            temp += d.get(i)
-        if(temp == t):
-            return True
-        return False
+            if(i in d and d.get(i)!=t[j]):
+                return False
+            d[i] = t[j]
+            j+=1
+        d2={}
+        j = 0
+        for i in t:
+            if(i in d2 and d2.get(i)!=s[j]):
+                return False
+            d2[i] = s[j]
+            j+=1
+        return True

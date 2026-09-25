0b25510369c95e9e6a133807250f0cd15349549d
Unnati Mishra <unnatiald@gmail.com>
2026-09-25 16:46:55 +0530

    Time: 325 ms (77.7%), Space: 68.4 MB (58.73%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.py
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
     df = pd.merge(employee,bonus,on = 'empId',how='left')
-    return df[(df['bonus']<1000) | df['bonus'].isnull()].drop(columns=['empId','supervisor','salary'])
+    return df[(df['bonus']<1000) | df['bonus'].isnull()][['name','bonus']]
